83c83ea4f7d3cd9d3ad7b5f5ad4a33f41783fbda
omvrs <omarsidqy5@gmail.com>
2026-01-12 16:04:19 +0100

    Add files via upload

---
+++ php_revision.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$Suniversité = [
+    "GL" => [
+        "PHP" => [
+            ["nom" => "Etudiant1", "note" => 15],
+            ["nom" => "Etudiant2", "note" => 16]
+        ],
+        "POO" => [
+            ["nom" => "Etudiant1", "note" => 14],
+            ["nom" => "Etudiant2", "note" => 17]
+        ]
+    ],
+    "RT" => [
+        "RESEAUX" => [
+            ["nom" => "Etudiant3", "note" => 13],
+            ["nom" => "Etudiant4", "note" => 18]
+        ],
+        "SECURITE" => [
+            ["nom" => "Etudiant3", "note" => 12],
+            ["nom" => "Etudiant4", "note" => 16]
+        ]
+    ]
+];
+?>
+</body>
+</html>
